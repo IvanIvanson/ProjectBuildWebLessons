c05ce1b54c42bdb9c4953311376af213d9bdc8c2
IvanIvanson <ghalut1977@gmail.com>
2022-08-30 21:58:26 +0300

    lesson1 задание1

--- lesson-1/dateDiff/js/printResult.js
+++ lesson-1/dateDiff/js/printResult.js
@@ -1,9 +1,12 @@
 const result = document.getElementById('datecalc__result')
+const outTime = document.getElementById("out");
 
 export const printError = (errorText) => {
   result.innerText = errorText
 }
-
+export const printErrorTime = (errorText) => {
+  outTime.innerText = errorText;
+};
 export const printResult = ({ years, months, days }) => {
   result.innerText = `Год: ${years} - Месяц: ${months} - День: ${days}`
 } 
